ab189de0030eb2488731a10abdf979f4ab6a0051
Milan Beran <milan.beran@roadsurfer.com>
2023-12-21 10:57:12 +0100

    puzzle 19 part 2

--- src/Puzzle19.php
+++ src/Puzzle19.php
@@ -13,14 +13,12 @@ class Puzzle19 extends AbstractPuzzle
     private const REJECTED = 'R';
 
     private array $accepted = [];
+    private array $rejected = [];
 
     private const INPUT_RULE = 'in';
     public function runPart01(): int
     {
         $this->loadData();
-
-//        print_r($this->rules);
-//        print_r($this->input);
 
         return $this->calculate();
     }
@@ -88,7 +86,93 @@ class Puzzle19 extends AbstractPuzzle
     {
         $this->loadData();
 
-        return 0;
+        $group = [
+            'x' => [
+                'min' => 1,
+                'max' => 4000,
+            ],
+            'm' => [
+                'min' => 1,
+                'max' => 4000,
+            ],
+            'a' => [
+                'min' => 1,
+                'max' => 4000,
+            ],
+            's' => [
+                'min' => 1,
+                'max' => 4000,
+            ],
+            'rule' => self::INPUT_RULE,
+        ];
+
+        $this->calculateGroups($group);
+
+        $total = 0;
+
+        foreach ($this->accepted as $item) {
+            $total += $this->calculateGroup($item);
+        }
+
+        return $total;
+    }
+
+    private function calculateGroup(array $group): int
+    {
+        return ($group['x']['max'] - $group['x']['min'] + 1)
+            * ($group['m']['max'] - $group['m']['min'] + 1)
+            * ($group['a']['max'] - $group['a']['min'] + 1)
+            * ($group['s']['max'] - $group['s']['min'] + 1);
+    }
+
+    private function calculateGroups(array $initGroup): void
+    {
+        $groups = [$initGroup];
+
+        while ($group = array_shift($groups)) {
+            $rule = $group['rule'];
+            foreach ($this->rules[$rule] as $condition) {
+                $operation = $condition['operation'] ?? null;
+                $variable = $condition['variable'] ?? null;
+                $destination = $condition['destination'];
+
+                switch ($operation) {
+                    case self::BIGGER_THAN:
+                        $newGroup1 = $group;
+                        $newGroup2 = $group;
+                        $newGroup1[$variable]['min'] = $condition['amount'] + 1;
+                        $newGroup2[$variable]['max'] = $condition['amount'];
+                        break;
+                    case self::SMALLER_THAN:
+                        $newGroup1 = $group;
+                        $newGroup2 = $group;
+                        $newGroup1[$variable]['max'] = $condition['amount'] - 1;
+                        $newGroup2[$variable]['min'] = $condition['amount'];
+                        break;
+                    default:
+                        if ($destination === self::ACCEPTED) {
+                            $this->accepted[] = $group;
+                        } elseif ($destination === self::REJECTED) {
+                            $this->rejected[] = $group;
+                        } else {
+                            $group['rule'] = $condition['destination'];
+                            $groups[] = $group;
+                        }
+                        break 2;
+                }
+
+                $group = $newGroup2;
+
+                if ($destination === self::ACCEPTED) {
+                    $this->accepted[] = $newGroup1;
+                } elseif ($destination === self::REJECTED) {
+                    $this->rejected[] = $newGroup1;
+                } else {
+                    $newGroup1['rule'] = $condition['destination'];
+                    $groups[] = $newGroup1;
+                }
+            }
+        }
     }
 
     private function loadData(): void
